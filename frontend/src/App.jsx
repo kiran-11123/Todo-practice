7bca0930b57ee38fecbae6f499c2c046eafa44c9
Kiran <kiran.sadara6730@gmail.com>
2025-11-25 21:31:31 +0530

    Frontend Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const[data, setData] = useState([])
+  
+  useEffect(()=>{
+
+    async function getData() {
+
+       try{
+      const response = await axios.get('http://localhost:5000/api/data')
+
+      if(response.status === 200) {
+        console.log('Data from backend:', response.data)
+        setData(response.data)
+      } else {
+        console.error('Failed to fetch data from backend')
+
+      }
+    }
+    catch(error) {
+      console.error('Error fetching data from backend:', error)
+      
+    }
+
+  }  getData()
+
+  },[])
 
   return (
      <div className='text-center bg-red-200'>
-          <h1>Welcome to the Frontend Application</h1>
+          {data.length > 0 ? (
+            <ul>
+              {data.map((item, index) => ( 
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>No data available</p>  
+               )}
      </div>
   )
 }
