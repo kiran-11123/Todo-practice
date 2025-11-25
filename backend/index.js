5fda6492d0c37ad18e22e98b7f2ae2b7828ae48f
Kiran <kiran.sadara6730@gmail.com>
2025-11-25 20:29:02 +0530

    GET API created for Todo App

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import connectDB from './models/db.js';
+import Todo_Router from './routes/main.js';
 
 connectDB();
 const app = express();
 app.use(express.json());
-
+app.use("/api/v1" , Todo_Router)
 
 
 
